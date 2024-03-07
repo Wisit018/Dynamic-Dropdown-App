71a6850207be7356a46289b280f467e943f0cf54
Wisit018 <lipton_pink@hotmail.co.th>
2024-03-07 14:31:31 +0700

    Day 1 | Finish Project :+1:

---
+++ dynamic-dropdown/src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import DropdownComponent from "./components/DropdownComponent";
+import FoodComponent from "./components/FoodComponent";
+import { useState } from "react";
+import MenuData from "./data/MenuData";
+function App() {
+  const [foods, setFoods] = useState(MenuData);
+
+  const changeFoodData = (e) => {
+    const category = e.target.value;
+
+    if (category === "เมนูทั้งหมด") {
+      setFoods(MenuData);
+    } else {
+      const result = MenuData.filter((Element) => {
+        return Element.menu === category;
+      });
+      setFoods(result);
+    }
+  };
+
+  return (
+    <div className="container">
+      <DropdownComponent changeFoodData={changeFoodData} />
+      <div className="content">
+        {foods.map((data, index) => {
+          return <FoodComponent key={index} {...data} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
